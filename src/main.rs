6e791adc316c92c8a7d41be2d137943d24c097e3
Huan Min Gan <17909473+ministrudels@users.noreply.github.com>
2023-08-17 23:41:09 +0100

    initial single threaded

---
+++ src/main.rs
@@ -0,0 +1,36 @@
+use std::fs;
+use std::io::prelude::*;
+use std::net::{TcpListener, TcpStream};
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        handle_connection(stream);
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer: [u8; 1024] = [0; 1024];
+
+    stream.read(&mut buffer).unwrap();
+    // Expected header for HTTP request
+    let get = b"GET / HTTP/1.1\r\n";
+
+    let (status_line, filename) = if buffer.starts_with(get) {
+        // Play with HTTP3
+        ("HTTP/3.1 200 OKP", "index.html")
+    } else {
+        ("HTTP/3.1 404 OKP", "404.html")
+    };
+
+    let contents = fs::read_to_string(filename).unwrap();
+    let response = format!(
+        "{}\r\nContent-Length: {}\r\n\r\n{}",
+        status_line,
+        contents.len(),
+        contents
+    );
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
+}
