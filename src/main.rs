e40f3d270ea2e8a0ec8718625c4b689ab406f56c
Huan Min Gan <17909473+ministrudels@users.noreply.github.com>
2023-08-18 19:52:10 +0100

    multithreaded

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,22 @@
-use std::fs;
-use std::io::prelude::*;
-use std::net::{TcpListener, TcpStream};
+use std::{
+    fs,
+    io::prelude::*,
+    net::{TcpListener, TcpStream},
+    thread,
+    time::Duration,
+};
+mod threadpool;
+use threadpool::ThreadPool;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
+
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        handle_connection(stream);
+        pool.execute(|| {
+            handle_connection(stream);
+        });
     }
 }
 
@@ -14,11 +24,15 @@ fn handle_connection(mut stream: TcpStream) {
     let mut buffer: [u8; 1024] = [0; 1024];
 
     stream.read(&mut buffer).unwrap();
-    // Expected header for HTTP request
+    // Expected request path and headers for HTTP request
     let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
 
+    // Play with HTTP3
     let (status_line, filename) = if buffer.starts_with(get) {
-        // Play with HTTP3
+        ("HTTP/3.1 200 OKP", "index.html")
+    } else if buffer.starts_with(sleep) {
+        thread::sleep(Duration::from_secs(5));
         ("HTTP/3.1 200 OKP", "index.html")
     } else {
         ("HTTP/3.1 404 OKP", "404.html")
